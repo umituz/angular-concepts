68200d775456a76914887e8d6c45c10fb4c1aeb7
Ümit UZ <umituz998@gmail.com>
2020-04-06 23:41:36 +0300

    ngOnInit lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'umituz';
+
+  message(value) {
+    console.log(value);
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit() {
+    this.message(this.title);
+  }
+
 }
