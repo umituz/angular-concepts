a96cb20c6b91ce75ba61601c3bd6082ed568592d
Ümit UZ <umituz998@gmail.com>
2020-04-07 00:55:55 +0300

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,29 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
 import {PokemonComponent} from './pokemon/pokemon.component';
 import {FormsModule} from '@angular/forms';
-import { MarvelHeroesComponent } from './marvel-heroes/marvel-heroes.component';
+import {MarvelHeroesComponent} from './marvel-heroes/marvel-heroes.component';
+import {DcComicsComponent} from './dc-comics/dc-comics.component';
+
+const routeLists: Routes = [
+  {path: '', component: DcComicsComponent},
+  {path: 'marvels', component: MarvelHeroesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonComponent,
-    MarvelHeroesComponent
+    MarvelHeroesComponent,
+    DcComicsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routeLists)
   ],
   providers: [],
   bootstrap: [AppComponent]
