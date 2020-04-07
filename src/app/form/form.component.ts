38e8df28029b757d226db7ec5f162d8f571a6c9f
Ümit UZ <umituz998@gmail.com>
2020-04-07 23:14:37 +0300

    form elements created

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  hide = true;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,20 +2,25 @@ import {NgModule} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
 
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
   ],
 
   exports: [
     MatToolbarModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
   ]
 
 })
